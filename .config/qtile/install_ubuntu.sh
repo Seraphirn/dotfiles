ba466d358d5adaea63790197e4a9d671aaf61030
Sergey Yukhimets <s.s.yukhimets@gmail.com>
2025-12-06 20:38:19 +0400

    qtile

---
+++ .config/qtile/install_ubuntu.sh
@@ -0,0 +1,7 @@
+
+sudo apt install xserver-xorg xinit
+sudo apt install uv  # if your distro doesn't have uv, try:
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+
+sudo apt install python3-cffi  python3-cairocffi  libpangocairo-1.0-0 python3-xcffib libiw-dev
+uv tool install "qtile[widgets]"
---
+++ .config/qtile/startup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#red light filter
+redshift -l 55.7615902:37.60946 -t 3400:3400 -m randr &>/dev/null &
+
+#wallpaper
+# feh --bg-scale wallpaper.jpg
+
+#trayer --edge bottom --align right --SetDockType true \
+#--SetPartialStrut true --expand true --widthtype percent \
+#--width 100 --height 25 &>/dev/null &
+
+sudo nm-applet &>/dev/null &
+
+# screen locker
+#xautolock -time 25 -detectsleep -locker xsecurelock &>/dev/null &
+
+# set minimal light of screen
+light -N 20
+
+# mouse sensetivity
+xinput set-prop 10 "libinput Accel Speed" +0.9
+# tapping enabled
+xinput set-prop 10 "libinput Tapping Enabled" 1
+
+# layout change
+# setxkbmap -layout "us,ru" -option "grp:alt_space_toggle"
+
+# exec program on start
+
+#exec $BROWSER &>/dev/null &
+#exec $TERMINAL -e $EDITOR &>/dev/null &
+#exec $TERMINAL &>/dev/null &
