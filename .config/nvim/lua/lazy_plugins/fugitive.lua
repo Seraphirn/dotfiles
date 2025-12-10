5d480734b4ddbb5193dabff48ac8c239ae377cc8
Sergey Yukhimets <s.s.yukhimets@gmail.com>
2025-12-10 23:20:20 +0400

    nvim: adds fugitive + yazi preview keys

---
+++ .config/nvim/lua/lazy_plugins/fugitive.lua
@@ -0,0 +1,12 @@
+return {
+    'tpope/vim-fugitive',
+    lazy = false,
+    -- cmd = { 'Git', 'Gdiff', 'Gblame', 'Glog', 'Gread', 'Gwrite', 'Ggrep' },
+    keys = {
+        -- { '<leader>gs', '<cmd>Git<cr>', desc = 'Open Git status' },
+        -- { '<leader>gd', '<cmd>Gdiff<cr>', desc = 'Open Git diff' },
+        -- { '<leader>gb', '<cmd>Gblame<cr>', desc = 'Open Git blame' },
+    },
+    config = function()
+    end,
+}
