7fbfadb3831a69667c614bd692675fc680e86efc
Sergey Yukhimets <s.s.yukhimets@gmail.com>
2025-12-12 01:10:10 +0400

    nvim: hop is now fully motion

--- .config/nvim/lua/lazy_plugins/hop.lua
+++ .config/nvim/lua/lazy_plugins/hop.lua
@@ -1,9 +1,9 @@
 return { -- Easy_motion alternative. fast travel thought code
     'smoka7/hop.nvim',
     keys = {
-        { '\\', ':HopWord<cr>', mode = '', desc = 'Hop' },
-        { '<Tab>', ':HopChar2<cr>', mode = '', desc = 'Hop' },
-        { '<Backspace>', ':HopCamelCase<cr>', mode = '', desc = 'Hop' },
+        { '\\', '<cmd>HopWord<cr>', mode = '', desc = 'Hop' },
+        { '<Tab>', '<cmd>HopChar2<cr>', mode = '', desc = 'Hop' },
+        { '<Backspace>', '<cmd>HopCamelCase<cr>', mode = '', desc = 'Hop' },
     },
     opts = {
         keys = 'etovxqpdygfblzhckisuran',
