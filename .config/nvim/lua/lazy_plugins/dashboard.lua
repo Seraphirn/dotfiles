7d9a3b93ca006459576bc4f0176523231d7f2628
Sergey Yukhimets <s.s.yukhimets@gmail.com>
2025-12-12 00:44:16 +0400

    nvim: dashboard

---
+++ .config/nvim/lua/lazy_plugins/dashboard.lua
@@ -0,0 +1,36 @@
+return {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup {
+            theme = 'hyper',
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    -- { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                    { desc = '󰊳 Empty', group = '@property', action = 'enew', key = 'e' },
+                    {
+                        icon = ' ',
+                        icon_hl = '@variable',
+                        desc = 'Projects',
+                        group = 'Label',
+                        action = 'NeovimProjectDiscover',
+                        key = 'p',
+                    },
+                    {
+                        desc = ' Config',
+                        group = 'Number',
+                        action = function()
+                            require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' }
+                        end,
+                        key = 'c',
+                    },
+                },
+            },
+        }
+        vim.keymap.set('n', '<leader>w', ':Dashboard<cr>', { desc = '[W]elcome screen' })
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
